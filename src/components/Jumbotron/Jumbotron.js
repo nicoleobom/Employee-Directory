23a285335503303b3214cda452c45ca470ea0512
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-07-08 09:37:46 -0400

    adding jumbotron component

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div class="container" align="center">
+                <h1 class="display-4">Employee Directory</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
