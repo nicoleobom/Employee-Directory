0ea1070bbf7229719fb8931eaaba914a223a21eb
Nicole OBomsawin <nicoleobomsawin@gmail.com>
2020-07-16 09:36:02 -0400

    revising table info

--- src/components/Columns/Columns.js
+++ src/components/Columns/Columns.js
@@ -1,19 +1,41 @@
 import React from "react";
 import "./Columns.css";
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import employees from '../employees.json';
 
-function Columns() {
-    return (
-        
-        <BootstrapTable className="table">
-            <TableHeaderColumn isKey dataField="id">Employee ID<i class="fas fa-sort"></i></TableHeaderColumn>
-            <TableHeaderColumn dataField="firstName">First Name<i class="fas fa-sort"></i></TableHeaderColumn>
-            <TableHeaderColumn dataField="lastName">Last Name<i class="fas fa-sort"></i></TableHeaderColumn>
-            <TableHeaderColumn dataField="gender">Gender</TableHeaderColumn>
-            <TableHeaderColumn dataField="age">Age<i class="fas fa-sort"></i></TableHeaderColumn>
-            <TableHeaderColumn dataField="emailAddress">Email Address</TableHeaderColumn>
-        </BootstrapTable>
-    )
+function revertSortFunc(a, b, order) {
+    if (order === 'desc') {
+        return a.id - b.id;
+    } else {
+        return b.id - a.id;
+    }
+}
+
+function sortFuncName(a, b, order) {
+    if (order === 'desc') {
+        return a.lastName - b.lastName;
+    } else {
+        return b.lastName - a.lastName;
+    }
+}
+
+class Columns extends React.Component {
+
+    
+
+    render() {
+        return (
+            
+            <BootstrapTable data={ employees }>
+                <TableHeaderColumn isKey dataField="id" dataSort sortFunc={ revertSortFunc }>Employee ID<i class="fas fa-sort"></i></TableHeaderColumn>
+                <TableHeaderColumn dataField="firstName">First Name<i class="fas fa-sort"></i></TableHeaderColumn>
+                <TableHeaderColumn dataField="lastName" dataSort sortFunc={ sortFuncName }>Last Name<i class="fas fa-sort"></i></TableHeaderColumn>
+                <TableHeaderColumn dataField="gender">Gender</TableHeaderColumn>
+                <TableHeaderColumn dataField="age">Age<i class="fas fa-sort"></i></TableHeaderColumn>
+                <TableHeaderColumn dataField="emailAddress">Email Address</TableHeaderColumn>
+            </BootstrapTable>
+        )
+    }
 }
 
 export default Columns;
