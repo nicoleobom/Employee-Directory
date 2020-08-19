da5cb612cbc675e5381fecff9398b5b849f91168
Nicole O’Bomsawin <nicoleobomsawin@gmail.com>
2020-08-19 18:23:20 -0400

    creating pop up component

--- src/components/AddBtn/index.js
+++ src/components/AddBtn/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import './index.css';
 
 class AddBtn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPopUp: false
+        };
+    }
+
+    togglePopUp() {
+        this.setState({
+            showPopUp: !this.state.showPopUp
+        });
+    }
+
     render() {
         return (
             <div>
-                <button><i class="fas fa-plus"></i></button>
+                <button onClick={this.togglePopUp.bind(this)}><i class="fas fa-plus"></i></button>
             </div>
         );
     }
---
+++ src/components/Popup/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Popup extends React.Component {
+
+}
+
+export default Popup;
